bbdc2880dbd92b30520df67b8a1cd5cad221fe9b
Max1mGarsh1n <maximgarshin@gmail.com>
2025-07-16 20:28:18 +0300

    addition to validation and functionality

--- core/src/main/java/org/project/core/dto/EventRequest.java
+++ core/src/main/java/org/project/core/dto/EventRequest.java
@@ -1,12 +1,20 @@
 package org.project.core.dto;
 
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@RequiredArgsConstructor
 public class EventRequest {
     public String title;
     public String description;
     public LocalDateTime dateTime;
+
+    public EventRequest(String s, String description, LocalDateTime futureDate) {
+        this.title = s;
+        this.description = description;
+        this.dateTime = futureDate;
+    }
 }
--- core/src/main/java/org/project/core/service/EventService.java
+++ core/src/main/java/org/project/core/service/EventService.java
@@ -2,6 +2,7 @@ package org.project.core.service;
 
 import org.project.core.dto.EventRequest;
 import org.project.core.entity.Event;
+import org.project.core.util.EventUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -17,12 +18,28 @@ public class EventService {
         return new ArrayList<>(events);
     }
 
+    public List<Event> getFutureEvents() {
+        return events.stream()
+                .filter(EventUtils::isFutureEvent)
+                .toList();
+    }
+
     public Event create(EventRequest req) {
         Event event = new Event(req.getTitle(), req.getDescription(), req.getDateTime());
+
+        if (!EventUtils.isValidTitle(event)) {
+            throw new IllegalArgumentException("Event title cannot be null or blank");
+        }
+
+        if (!EventUtils.isFutureEvent(event)) {
+            throw new IllegalArgumentException("Event date must be in the future");
+        }
+
         event.setId(++lastId);
         events.add(event);
         return event;
     }
+
 
     public Optional<Event> getById(Long id) {
         return events.stream()
